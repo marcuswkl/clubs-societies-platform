47d6f9bfa46ec800f26fcbe0480db1d5df779a18
marcuswkl <56200199+marcuswkl@users.noreply.github.com>
2020-11-15 20:58:34 +0800

    Initialise Firestore and get data

--- app/src/main/java/com/marcuswkl/cnsplatform/ui/club/ClubFragment.kt
+++ app/src/main/java/com/marcuswkl/cnsplatform/ui/club/ClubFragment.kt
@@ -4,8 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import com.marcuswkl.cnsplatform.R
 import com.marcuswkl.cnsplatform.ui.enquire.EnquireFragment
 import kotlinx.android.synthetic.main.fragment_club.view.*
@@ -22,6 +25,21 @@ class ClubFragment : Fragment() {
         clubViewModel =
             ViewModelProvider(this).get(ClubViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_club, container, false)
+
+        val db = Firebase.firestore
+
+        val clubRef = db.collection("clubs").document("sunwayuniversitystudentcouncil")
+        clubRef.get()
+            .addOnSuccessListener { document ->
+                if (document != null) {
+                    Toast.makeText(activity, "Read Document Successful.", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(activity, "Document Does Not Exist.", Toast.LENGTH_SHORT).show()
+                }
+            }
+            .addOnFailureListener { exception ->
+                Toast.makeText(activity, "Read Failed. $exception", Toast.LENGTH_SHORT).show()
+            }
 
         root.club_enquire_button.setOnClickListener {
             val enquireFragment = EnquireFragment()
