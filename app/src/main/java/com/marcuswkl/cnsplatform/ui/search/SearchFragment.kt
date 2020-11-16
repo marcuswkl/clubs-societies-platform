4088954666c8bb89079bb17db0e204c75c5ac44e
marcuswkl <56200199+marcuswkl@users.noreply.github.com>
2020-11-17 03:46:04 +0800

    Get user query from search field

--- app/src/main/java/com/marcuswkl/cnsplatform/ui/search/SearchFragment.kt
+++ app/src/main/java/com/marcuswkl/cnsplatform/ui/search/SearchFragment.kt
@@ -1,9 +1,11 @@
 package com.marcuswkl.cnsplatform.ui.search
 
 import android.os.Bundle
+import android.view.KeyEvent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.marcuswkl.cnsplatform.R
@@ -22,6 +24,29 @@ class SearchFragment : Fragment() {
         searchViewModel =
             ViewModelProvider(this).get(SearchViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_search, container, false)
+
+        root.search_field.setOnKeyListener(object : View.OnKeyListener {
+            override fun onKey(v: View?, keyCode: Int, event: KeyEvent?): Boolean {
+
+                if (event != null) {
+                    // When Enter key is pressed
+                    if (event.action == KeyEvent.ACTION_DOWN && keyCode == KeyEvent.KEYCODE_ENTER) {
+
+                        // Perform action
+
+                        val query = root.search_field.text
+                        Toast.makeText(activity, "$query", Toast.LENGTH_SHORT).show()
+
+                        return true
+
+                    }
+                }
+
+                return false
+
+            }
+
+        })
 
         root.leadership_tile.setOnClickListener {
             val leadershipFragment = LeadershipFragment()
