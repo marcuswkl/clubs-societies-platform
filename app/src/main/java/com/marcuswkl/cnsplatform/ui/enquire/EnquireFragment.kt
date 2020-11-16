e8c2823b6c250d61c087cb38263398603349e3f3
marcuswkl <56200199+marcuswkl@users.noreply.github.com>
2020-11-16 15:31:45 +0800

    Add enquire function

--- app/src/main/java/com/marcuswkl/cnsplatform/ui/enquire/EnquireFragment.kt
+++ app/src/main/java/com/marcuswkl/cnsplatform/ui/enquire/EnquireFragment.kt
@@ -7,11 +7,14 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.google.firebase.Timestamp
+import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import com.marcuswkl.cnsplatform.R
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.fragment_enquire.view.*
+import java.util.*
 
 class EnquireFragment : Fragment() {
 
@@ -38,8 +41,39 @@ class EnquireFragment : Fragment() {
                 clubRef.get()
                     .addOnSuccessListener { document ->
                         if (document != null) {
+
                             Picasso.get().load(document.getString("logo")).into(root.enquire_club_logo)
                             root.enquire_club_name_title.text = document.getString("name")
+
+                            root.enquire_submit_button.setOnClickListener {
+
+                                val user = Firebase.auth.currentUser
+                                val iMail = user?.email
+                                val message = root.enquire_field.text.toString()
+
+                                if (message.isEmpty()) {
+                                    Toast.makeText(activity, "Invalid Message", Toast.LENGTH_SHORT).show()
+                                } else {
+
+                                    val enquiryData = hashMapOf(
+                                        "imail" to iMail,
+                                        "message" to message,
+                                        "timestamp" to Timestamp(Date())
+                                    )
+
+                                    clubRef.collection("enquiries")
+                                        .add(enquiryData)
+                                        .addOnSuccessListener {
+                                            Toast.makeText(activity, "Submission Success", Toast.LENGTH_SHORT).show()
+                                        }
+                                        .addOnFailureListener {
+                                            Toast.makeText(activity, "Submission Fail", Toast.LENGTH_SHORT).show()
+                                        }
+
+                                }
+
+                            }
+
                         } else {
                             Toast.makeText(activity, "Document Does Not Exist.", Toast.LENGTH_SHORT).show()
                         }
